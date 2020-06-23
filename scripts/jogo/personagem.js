d2c9ff14eec8dec0679d96a233321c690b94dc28
Henrique Fernandes <henrique.fquick@gmail.com>
2020-06-23 20:05:15 -0300

    Added and updates files ( second class )

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,33 +1,28 @@
-class Personagem{
-  constructor(imagem){
-      this.imagem = imagem;
-      this.matriz = [
-        [0,0],
-        [220,0],
-        [440,0],
-        [660,0],
-        [0,270],
-        [220,270],
-        [440,270],
-        [660,270],
-        [0,540],
-        [220,540],
-        [440,540],
-        [660,540],
-        [0,810],
-        [220,810],
-        [440,810],
-        [660,810],
-      ];
-    this.frameAtual = 0;
+class Personagem extends Animacao{
+  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
+    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+
+    this.yInicial = height - this.altura;
+    this.y = this.yInicial;
+    
+    this.gravidade = 3;
+    this.velocidadeDoPulo = 0;
+  }  
+  pula(){
+    this.velocidadeDoPulo = -30;
   }
-  exibe(){
-    image(this.imagem, 0, height-135, 110, 135, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270);
-    this.anima();
+
+  aplicaGravidade(){
+    this.y = this.y + this.velocidadeDoPulo;
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+
+    if(this.y > this.yInicial)
+      this.y = this.yInicial
   }
-  anima(){
-    this.frameAtual = (this.frameAtual + 1) % this.matriz.length;
+
+  estaColidindo(inimigo){
+    const precisao = .7
+    return collideRectRect(this.x, this.y, this.largura*precisao, this.altura*precisao, inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
   }
-  
 }
 
